018f1cefb9d36084b50ba64caae2b716d75a90cb
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2025-07-05 14:13:01 -0700

    Add notification on grammar processing completion
    Introduced a notification utility using notifypy and integrated it into the grammar_service_callback to notify users when text processing is complete. Also simplified the prompt template in settings.json and added timing logs for function execution.

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -1,6 +1,8 @@
 from llama_cpp import Llama
 import pyperclip
 import json
+import time
+from utils.notification_utils import notification
 
 class LLM:
     def __init__(self):
@@ -16,10 +18,19 @@ class LLM:
         self.prev_coppied_text = ""
 
     def grammar_service_callback(self, clipboard_text: str):
+        print(f"Text proccessing Started!")
+        
+        start_time = time.time()
         if clipboard_text != self.prev_coppied_text and clipboard_text.strip() != "":
             prompt = self.prompt_template.format(text=clipboard_text)
             llm_processed_text = self.llm(prompt, max_tokens=self.max_tokens)
             self.prev_coppied_text = clipboard_text
             self.processed_text = llm_processed_text["choices"][0]["text"].strip().strip("'").strip('"')
             print(f"Text got proccessed!")
+
+            end_time = time.time()  # Record end time
+            duration = end_time - start_time
+            print(f"Function execution time: {duration:.3f} seconds")
+
             pyperclip.copy(self.processed_text)
+            notification.send(block=False)
---
+++ utils/notification_utils.py
@@ -0,0 +1,6 @@
+from notifypy import Notify
+
+notification = Notify(
+    default_notification_title= "HotKeyGrammarAid",
+    default_notification_message= "Operation Done!"
+)
